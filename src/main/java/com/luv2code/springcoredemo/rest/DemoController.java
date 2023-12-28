313c0a29b2edf07a99432a5270c851a7aa234058
amrassal707 <51443768+amrassal707@users.noreply.github.com>
2023-12-28 18:43:16 +0200

    Component-Scanning

---
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -0,0 +1,25 @@
+package com.luv2code.springcoredemo.rest;
+
+import com.luv2code.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach mycoach;
+
+    @Autowired // inject the dependency required // optional if only once constructor is there
+
+    public DemoController(Coach coach){
+        mycoach=coach;
+    }
+
+
+
+    @GetMapping ("getdailyworkout")
+    public String getdailyworkout(){
+        return mycoach.GetDailyWorkout();
+    }
+}
